cf9357879fe47071215700315e9f29af55ca4fe5
Christopher Nguyen <chris_ngu@pm.me>
2020-01-14 15:00:25 -0600

    bugfixing

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands, tasks
 import os
+import time
 
 client = commands.Bot(command_prefix= '.')
 
@@ -8,19 +9,19 @@ client = commands.Bot(command_prefix= '.')
 async def on_ready():
     print('Bot is ready')
 
-@client.event
-async def ping():
-    await ctx.send(f'Ping:  {round(client.latency * 1000)}ms')
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Ping: {round(client.latency * 1000)}ms')
 
-@client.event
+@client.command()
 async def clear(ctx, amount = 2):
     await ctx.channel.purge(limit = amount)
     await ctx.send(f'cleared {amount} messages')
-    time.sleep(5)
+    time.sleep(3)
     await ctx.channel.purge(limit = 1)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run("ENTER KEY HERE")
+client.run('NjU3Mzg1Mzk1MTA0OTA3MjY3.XhkBrw.B8LW6tDsEAkPhHedSZuQnCwTOkQ')
