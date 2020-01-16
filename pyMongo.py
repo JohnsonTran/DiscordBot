e4505f3958aea4dece1d116d70a323d69c2f1a02
Christopher Nguyen <chris_ngu@pm.me>
2020-01-15 21:02:33 -0600

    Fixing player stats

--- pyMongo.py
+++ pyMongo.py
@@ -11,13 +11,32 @@ collection = db["uno"]
 def finish_game():
     collection.update(
         { _id: ObjectId("5e1d16d5df6dc71021b31c92")},
-        { $inc: { total_games: 1 } } }
+        { $inc: { total_games: 1 } },
+        { $set: {last_played: Date()} }
     )
 
-#keeps track of previous winners and total times won, this might not work; not tested
+'''
 def add_winner(discord_id):
     collection.update(
         { _id: ObjectId("5e1d16d5df6dc71021b31c92")},
-        {$inc: winners[{discord_id: 1}]},
+        {$inc: winners[{discord_id: 1}] },
         {upsert: true}
     )
+    BUGFIXING THIS, LOOK AT BOTTOM'''
+
+#probably need to format this since discord should display BSON
+def get_stats(): 
+    collection.find(_id: ObjectId("5e1d16d5df6dc71021b31c92")).pretty()
+
+
+    #currently only sets, needs to inc
+    db.uno.update({ _id: ObjectId("5e1d16d5df6dc71021b31c92")},
+        {winners: [ {GruntyBunty: 1} ]},
+        {upsert: true}   
+    )
+
+       db.uno.update({ _id: ObjectId("5e1d16d5df6dc71021b31c92")},
+        {$inc:{"winners.GruntyBunty": 1}},
+        {upsert: true}
+    )
+   
